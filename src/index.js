32fe29dd7c759695e63ef77f7f9dcca5e0d53def
Juliana Ferreira Soares <julianajfs.adv@gmail.com>
2025-07-16 14:59:39 -0300

    atualizacao2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-console.log ("Olá mundo!");
+let nomeHeroi ="Juli";
+let experienciaXp = "";
+console.log("O herói " + nomeHeroi + " está no nível " + experienciaXp);
+switch (experienciaXp) { 
+    case 0 < 1000:
+        console.log ("Ferro");
+        break;
+    case 1001 < 2000:
+        console.log ("Bronze");
+        break;
+    case 2001 < 5000:
+        console.log ("Prata");
+        break;
+    case 5001 < 7000:
+        console.log ("Ouro");
+        break;
+    case 7001 < 8000:
+        console.log ("Platina")
+        break;
+    case 8001 < 9000:
+        console.log ("Ascendente");
+        break;
+    case 9001 < 10000:
+        console.log ("Imortal");
+        break;
+    default:
+        console.log ("Radiante");
+
+    
+
+
+    
+}
+
 
     
